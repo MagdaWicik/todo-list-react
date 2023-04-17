850d2b8063f0c54bda21fe9eb7925771520156f5
Magda Wicik <magdaa.wicik@gmail.com>
2023-04-17 09:31:09 +0200

    key added

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -4,9 +4,10 @@ const Tasks = ({ tasks, hideDoneTasks }) => (
     <ul className="tasks">
         {tasks.map(task => (
             <li
+                key={task.id}
                 className={`tasks__item${task.done && hideDoneTasks
-                        ? " tasks__item--hidden "
-                        : ""}`
+                    ? " tasks__item--hidden "
+                    : ""}`
                 }
             >
                 <button className="tasks__button tasks__button--toggleDone">
@@ -14,7 +15,7 @@ const Tasks = ({ tasks, hideDoneTasks }) => (
                 </button>
                 <span className={
                     `tasks__content${task.done ? " tasks__content--done " : ""}`
-                    }>
+                }>
                     {task.content}
                 </span>
                 <button className="tasks__button tasks__button--remove">
