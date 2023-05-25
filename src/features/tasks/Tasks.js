144175de15eff81285b258bb1a4eef5688fdf81d
Magda Wicik <magdaa.wicik@gmail.com>
2023-05-25 21:59:17 +0200

    application split into features

---
+++ src/features/tasks/Tasks.js
@@ -0,0 +1,54 @@
+import Form from "./Form";
+import TaskList from "./TaskList";
+import Buttons from "./Buttons";
+import Section from "../../common/Section";
+import Header from "../../common/Header";
+import { useState } from "react";
+import { useTasks } from "../../useTasks";
+
+function Tasks() {
+  const [hideDone, setHideDone] = useState(false);
+
+  const {
+    tasks,
+    removeTask,
+    toggleTaskDone,
+    setAllDone,
+    addNewTask,
+   } = useTasks();
+
+  const toggleHideDone = () => {
+    setHideDone(hideDone => !hideDone);
+  };
+
+  return (
+    <main>
+      <Header title="Lista zadań" />
+      <Section 
+      title="Dodaj nowe zadanie" 
+      body={<Form addNewTask={addNewTask} />} 
+      />
+
+      <Section
+      title="Lista zadań"
+      body={
+      <TaskList 
+      tasks={tasks} 
+      hideDone={hideDone} 
+      removeTask={removeTask}
+      toggleTaskDone={toggleTaskDone} 
+      />
+    }
+      extraHeaderContent={
+      <Buttons tasks={tasks} 
+      hideDone={hideDone} 
+      toggleHideDone={toggleHideDone}
+      setAllDone={setAllDone}
+      />
+    }
+      />
+    </main>
+  );
+}
+
+export default Tasks;
