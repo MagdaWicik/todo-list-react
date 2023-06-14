84043cf96aacdbe15fa246c36d96658a4ac92573
Magda Wicik <magdaa.wicik@gmail.com>
2023-06-14 22:48:48 +0200

    TaskList's index.js moved to TasksPage

---
+++ src/features/tasks/TasksPage/TaskList/index.js
@@ -0,0 +1,44 @@
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useLocation } from "react-router-dom";
+import { List, Item, Content, Button } from "./styled";
+import { selectTasksByQuery, selectHideDone, toggleTaskDone, removeTask } from "../../tasksSlice";
+import searchQueryParamName from "../searchQueryParamName";
+
+const TaskList = () => {
+    const location = useLocation();
+    const query = new URLSearchParams(location.search).get(searchQueryParamName);
+
+    const tasks = useSelector(state => selectTasksByQuery(state, query));
+    const hideDone = useSelector(selectHideDone);
+
+    const dispatch = useDispatch();
+
+    return (
+        <List>
+            {tasks.map(task => (
+                <Item
+                    key={task.id}
+                    hidden={task.done && hideDone}
+                >
+                    <Button
+                        toggleDone
+                        onClick={() => dispatch(toggleTaskDone(task.id))}
+                    >
+                        {task.done ? "✔" : ""}
+                    </Button>
+                    <Content done={task.done}>
+                        <Link to={`/zadania/${task.id}`}>{task.content}</Link>
+                    </Content>
+                    <Button
+                        remove
+                        onClick={() => dispatch(removeTask(task.id))}
+                    >
+                        🗑
+                    </Button>
+                </Item>
+            ))}
+        </List>
+    );
+}
+
+export default TaskList; 
