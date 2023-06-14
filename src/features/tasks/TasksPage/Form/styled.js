ede5ae77b31b6907144f8bcf1d6f57fa7653915f
Magda Wicik <magdaa.wicik@gmail.com>
2023-06-14 22:50:14 +0200

    Form's styled.js moved to TasksPage

--- src/features/tasks/TasksPage/Form/styled.js
+++ src/features/tasks/TasksPage/Form/styled.js
@@ -4,29 +4,19 @@ export const FormWrapper = styled.form`
     background-color: rgb(255, 255, 255);
     padding: 20px;
     display: flex;
-    justify-content: space-between;
 
     @media(max-width: 767px) {
         flex-wrap: wrap;
     }
 `;
 
-export const Input = styled.input`
-    padding: 10px;
-    width: 600px;
-    outline-color: rgb(215, 215, 215);
-
-    @media(max-width: 767px) {
-        flex-grow: 1;
-    }
-`;
-
 export const Button = styled.button`
     padding: 10px;
-    background-color: rgb(33, 114, 114);
-    color: rgb(248, 241, 241);
+    background-color: teal;
+    color: white;
     border: none;
     transition: 1s;
+    margin-left: 10px;
 
     @media(max-width: 767px) {
         flex-grow: 1;
