a26d96ec65975fdc0d9f1f02a6878c81f7426481
Magda Wicik <magdaa.wicik@gmail.com>
2023-06-15 09:32:23 +0200

    GlobalStyles.js added

---
+++ src/GlobalStyles.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+html {
+  box-sizing: border-box;
+}
+
+*, ::after, ::before {
+  box-sizing: inherit;
+}
+
+body {
+  font-family: 'Montserrat', sans-serif;
+  background-color: rgb(240, 240, 240);
+}
+`;
